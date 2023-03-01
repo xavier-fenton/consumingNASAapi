71d4f3af664c2a059dda2edd80fd5815e8ec90ce
xavier-fenton <xavierfenton99@gmail.com>
2023-03-01 16:35:38 +1300

    useeffect to get data,, notice url

--- src/components/NewComponent.tsx
+++ src/components/NewComponent.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
+import { setEnvironmentData } from 'worker_threads';
 
 
 const NewComponent = () => {
   const [data, setData] = useState();
 
+  
+
   useEffect(() => {
-    fetch('https://images-api.nasa.gov/search?q=apollo11').then(response => response.json()).then(data => console.log(data);
-    )
-    
-  }, []);
-  return (  );
+    fetch('https://images-api.nasa.gov/search?q=space&media_type=image')
+    .then(response => response.json())
+    .then((data) => {
+      const collectedData = data.collection.items
+      setData(collectedData)
+      console.log(collectedData)
+    })
+  }, [])
+
+
+  return (<>hello</>  );
 }
  
 export default NewComponent;
