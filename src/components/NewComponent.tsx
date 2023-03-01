9ce3a23ed717bf4e9538c8eb412592dde33597d5
xavier-fenton <xavierfenton99@gmail.com>
2023-03-01 20:25:40 +1300

    fix types

--- src/components/NewComponent.tsx
+++ src/components/NewComponent.tsx
@@ -1,51 +1,46 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-
-
+import * as React from 'react'
+import { useState, useEffect } from 'react'
 
 const NewComponent = () => {
-  const [data, setData] : Array<any> = useState();
-
-  
+  const [data, setData]: Array<any> = useState()
 
   useEffect(() => {
     fetch('https://images-api.nasa.gov/search?q=space&media_type=image')
-    .then(response => response.json())
-    .then((data) => {
-      const collectedData = data.collection.items
-      // setData(collectedData)
-      const mappedData = collectedData.map((data: {links: string}) => {
-        return data.links
+      .then((response) => response.json())
+      .then((data) => {
+        const collectedData = data.collection.items
+        // setData(collectedData)
+        const mappedData = collectedData.map((data: { links: string }) => {
+          return data.links
+        })
+        const mappedData2 = mappedData.map((data: Array<any>) => {
+          const firstIndex: {href: string} = data[0]
+          const intoIndex = firstIndex.href
+          return intoIndex
+        })
+        setData(mappedData2)
       })
-      const mappedData2 = mappedData.map((data: Array<string>) => {
-        
-        const firstIndex = data[0]
-        const intoIndex = firstIndex.href
-        return intoIndex
-      })
-     setData(mappedData2)
-    
-      
-    })
   }, [])
 
-
   return (
-  
-  <>
-  <h1>NASA Image Data</h1>
-  {data && data.map((data) => {
-
-    return( 
     <>
-    <img className='image' src={data} id={data.id}alt="APOD" key={data.id} />
-    </>)
-  })}
-  
-  
-  
-  </>  
-  );
+      <h1>NASA Image Data</h1>
+      {data &&
+        data.map((data: {id: any} ) => {
+          return (
+            <>
+              <img
+                className="image"
+                src={data}
+                id={data.id}
+                alt="APOD"
+                key={data.id}
+              />
+            </>
+          )
+        })}
+    </>
+  )
 }
- 
-export default NewComponent;
+
+export default NewComponent
